19aa4b74e9f35a40ec4bb3798f9aa01247b2c60b
moizmalik11 <moiz.malik1113@gmail.com>
2025-05-19 20:24:31 +0500

    userrouters

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,3 +7,9 @@ router.post('/register', registerUser);
 module.exports = router;
 
 console.log("hello");
+// This is a simple Express route for user registration.
+// It imports the necessary modules, sets up a router, and defines a POST route for user registration.  
+//write a loop that prints "hello" 10 times
+for (let i = 0; i < 10; i++) {
+    console.log("hello");
+}
