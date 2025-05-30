46930e0688259a51ea92ad2264cf46f5a29742a8
moizmalik11 <moiz.malik1113@gmail.com>
2025-05-31 03:35:48 +0500

    commit socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -5,4 +5,9 @@ const socket = io("http://localhost:5000", {
   transports: ["websocket"],
 });
 
+// Optional: Add event listeners for connection and disconnection
+socket.on("connect", () => {
+  console.log("Connected to the server");
+});
+
 export default socket;
